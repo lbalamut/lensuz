dfe7a77722caeec7ebb4de8bd7b0311f1ca62fde
Lukasz Balamut <lukasz.balamut@nokia.com>
2012-06-03 00:54:03 +0200

    updated dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -2,39 +2,39 @@ import sbt._
 import Keys._
 
 object BuildSettings {
-  val buildOrganization = "com.nokia"
-  val buildVersion      = "0-SNAPSHOT"
-  val buildScalaVersion = "2.9.1"
+    val buildOrganization = "eu.balamut"
+    val buildVersion      = "0-SNAPSHOT"
+    val buildScalaVersion = "2.9.1"
 
-  val buildSettings = Defaults.defaultSettings ++ Seq(
-    organization  := buildOrganization,
-    version       := buildVersion,
-    scalaVersion  := buildScalaVersion,
-    resolvers += ("sbt-idea-repo" at "http://mpeltonen.github.com/maven/"),
-    scalacOptions ++= Seq("-unchecked", "-deprecation", "-encoding", "UTF8"),
-    javacOptions  ++= Seq("-g", "-encoding", "UTF8"))
+    val buildSettings = Defaults.defaultSettings ++ Seq(
+        organization  := buildOrganization,
+        version       := buildVersion,
+        scalaVersion  := buildScalaVersion,
+        resolvers += ("sbt-idea-repo" at "http://mpeltonen.github.com/maven/"),
+        scalacOptions ++= Seq("-unchecked", "-deprecation", "-encoding", "UTF8"),
+        javacOptions  ++= Seq("-g", "-encoding", "UTF8"))
 }
 
 object Dependencies {
-  val deps = Seq(
-    "org.scalaz" %% "scalaz-core" % "6.0.2",
-    "junit" % "junit" % "4.8.2" % "test",
-    "org.hamcrest" % "hamcrest-core" % "1.2" % "test",
-    "org.mockito" % "mockito-core" % "1.8.5" % "test"
-  )
+    val deps = Seq(
+        "org.scalaz" %% "scalaz-core" % "6.0.4",
+        "junit" % "junit" % "4.8.2" % "test",
+        "org.scalatest" %% "scalatest" % "1.8" % "test",
+        "org.scala-tools.testing" %% "specs" % "1.6.9" % "test"
+    )
 
-  val junit4SBT = "com.novocode" % "junit-interface" % "0.7" % "test->default"
+    val junit4SBT = "com.novocode" % "junit-interface" % "0.7" % "test->default"
 }
 
 object WS extends Build {
-  import Dependencies._
-  import BuildSettings._
+    import Dependencies._
+    import BuildSettings._
 
-  lazy val root =
+    lazy val root =
     Project("root", file("."),
-        settings = buildSettings ++ Seq(libraryDependencies ++= (deps :+junit4SBT))
-          :+ (parallelExecution in Test := false)
-          :+ (classDirectory in Compile <<= target in Compile apply { _ / "classes" })
-          :+ (classDirectory in Test <<= target in Test apply { _ / "test-classes" })
-  )
+    settings = buildSettings ++ Seq(libraryDependencies ++= (deps :+junit4SBT))
+    :+ (parallelExecution in Test := false)
+    :+ (classDirectory in Compile <<= target in Compile apply { _ / "classes" })
+    :+ (classDirectory in Test <<= target in Test apply { _ / "test-classes" })
+)
 }
