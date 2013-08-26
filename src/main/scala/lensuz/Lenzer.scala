c4f1c5c9ee77cb268ccfae1516a3dc5037b65374
Lukasz Balamut <lukasz.balamut@here.com>
2013-08-26 12:32:00 +0200

    untyped implementation of lenzer for all fields

---
+++ src/main/scala/lensuz/Lenzer.scala
@@ -0,0 +1,139 @@
+package lensuz
+
+
+import language.experimental.macros
+
+import scalaz.Lens
+
+object Lenzer {
+    def forField[S, T](fieldName: String): Lens[S, T] = macro LenzerMacros.forField[S, T]
+
+    def forAllFields[T]: AnyRef = macro LenzerMacros.forAllFields[T]
+}
+
+object LenzerMacros {
+
+    def forField[S: c.WeakTypeTag, T: c.WeakTypeTag]
+    (c: scala.reflect.macros.Context)
+    (fieldName: c.Expr[String]): c.Expr[scalaz.Lens[S, T]] = {
+
+        import c.universe._
+
+        def abort(reason: String) = c.abort(c.enclosingPosition, reason)
+
+        val lens = (c.prefix.tree, fieldName.tree) match {
+            case (x, Literal(Constant(name: String))) =>
+                val sourceT = c.weakTypeOf[S]
+
+                val mamber = sourceT.member(newTermName(name)) orElse {
+                    abort(s"value $name is not a member of $sourceT")
+                }
+
+                val targetT = mamber.typeSignatureIn(sourceT) match {
+                    case NullaryMethodType(tpe) => tpe
+                    case _ => abort(s"member $name is not a field")
+                }
+
+                lensForMember(c)(name, sourceT, targetT)
+
+            case x => abort(s"unexpected c.prefix tree: $x")
+        }
+
+        c.Expr[scalaz.Lens[S, T]](c.resetAllAttrs(lens))
+    }
+
+
+
+    private def lensForMember
+                    (c: scala.reflect.macros.Context)
+                    (name: String, sourceT: c.Type, targetT: c.Type): c.universe.Block = {
+
+        import c.universe._
+        val lens = Select(Ident(newTermName("scalaz")), newTermName("Lens"))
+        val lensu = Select(lens, newTermName("lensu"))
+
+        Block(
+            List(
+
+                ValDef(Modifiers(), newTermName(name + "GetF"), TypeTree(),
+                    Function(
+                        List(ValDef(Modifiers(Flag.PARAM), newTermName("p"), TypeTree(sourceT), EmptyTree)),
+                        Select(Ident(newTermName("p")), newTermName(name))
+                    )
+                ),
+
+                ValDef(Modifiers(), newTermName(name + "SetF"), TypeTree(),
+                    Function(
+                        List(ValDef(Modifiers(Flag.PARAM), newTermName("p"), TypeTree(sourceT), EmptyTree),
+                            ValDef(Modifiers(Flag.PARAM), newTermName("a"), TypeTree(targetT), EmptyTree)
+                        ),
+                        Apply(
+                            Select(Ident(newTermName("p")), newTermName("copy")),
+                            List(AssignOrNamedArg(Ident(newTermName(name)), Ident(newTermName("a"))))
+                        )
+                    )
+                )
+
+//                DefDef(Modifiers(), newTermName(name + "GetF"), List(),
+//                    List(List(ValDef(Modifiers(Flag.PARAM), newTermName("p"), TypeTree(sourceT), EmptyTree))),
+//                    TypeTree(targetT),
+//                    Select(Ident(newTermName("p")), newTermName(name))
+//                ),
+//
+//                DefDef(Modifiers(), newTermName(name + "SetF"), List(),
+//                    List(List(ValDef(Modifiers(Flag.PARAM), newTermName("p"), TypeTree(sourceT), EmptyTree), ValDef(Modifiers(Flag.PARAM), newTermName("i"), TypeTree(targetT), EmptyTree))),
+//                    TypeTree(sourceT),
+//                    Apply(Select(Ident(newTermName("p")), newTermName("copy")),
+//                        List(AssignOrNamedArg(Ident(newTermName(name)), Ident(newTermName("i"))))
+//                    )
+//                )
+            ),
+
+            Apply(
+                 lensu, List(Ident(newTermName(name + "SetF")), Ident(newTermName(name + "GetF")))
+            )
+        )
+    }
+
+    def forAllFields[T: c.WeakTypeTag](c: scala.reflect.macros.Context): c.Expr[AnyRef] = {
+        import c.universe._
+
+        val sourceT = c.weakTypeOf[T]
+
+        val valLensez:List[ValOrDefDef] = sourceT.declarations.collect {
+            case m: MethodSymbol if m.isCaseAccessor =>
+                val (name, targetT) = (m.name.toString, m.returnType)
+                val lensTypeTree = AppliedTypeTree(Ident(c.mirror.staticClass("scalaz.LensFamily")), List(TypeTree(sourceT), TypeTree(sourceT), TypeTree(targetT), TypeTree(targetT)))
+
+//                DefDef(Modifiers(Flag.PROTECTED), newTermName(name), List(), List(),
+//                    lensTypeTree,
+//                    lensForMember(c)(name, sourceT, targetT)
+//                )
+
+                ValDef(Modifiers(Flag.PROTECTED), newTermName(name),
+                    lensTypeTree,
+                    lensForMember(c)(name, sourceT, targetT)
+                )
+
+        }.toList
+
+        val lenzerObject = Block(
+            List(
+                ClassDef(Modifiers(Flag.FINAL), newTypeName("$anon"), List(),
+                    Template(
+                        List(Ident(c.mirror.staticClass("java.lang.Object"))),
+                        emptyValDef,
+
+                        (DefDef(Modifiers(), nme.CONSTRUCTOR, List(), List(List()), TypeTree(), Block(List(Apply(Select(Super(This(tpnme.EMPTY), tpnme.EMPTY), nme.CONSTRUCTOR), List())), Literal(Constant(())))) :: valLensez)
+                    )
+                )
+            ),
+            Apply(Select(New(Ident(newTypeName("$anon"))), nme.CONSTRUCTOR), List())
+        )
+
+//        println(lenzerObject)
+
+        c.Expr[AnyRef](c.resetAllAttrs(lenzerObject))
+    }
+}
+
---
+++ src/test/scala/lensuz/LenzerTest.scala
@@ -0,0 +1,54 @@
+package lensuz
+
+
+import org.scalatest.FunSuite
+
+import reflect.runtime.universe._
+
+import scalaz.Lens
+import scalaz.LensFamily
+import scala.language.reflectiveCalls
+
+case class Outer(name: String, inner: Inner)
+
+case class Inner(member: String)
+
+
+class LenzerTest extends FunSuite {
+
+    ignore("learning compiler AST") {
+        println(showRaw(reify {
+            new Object {
+//                val inner = Lens.lensu(
+//                    val get: (Outer) => Inner = (p: Outer) => p.inner
+//                    val set: (Outer, Inner) => Outer = (p: Outer, a: Inner) => p.copy(inner = a)
+//                    def nameGetF(p: lensuz.Outer): String = p.name
+//                    def nameSetF(p: lensuz.Outer, i: lensuz.Inner): Outer = p.copy(inner = i)
+                     def a: Lens[Outer, Inner] = null
+//                )
+            }
+        }.tree))
+
+    }
+
+    test("single field lens") {
+        val t = Outer(name = "name", inner = Inner("a"))
+
+        val innerL = Lenzer.forField[Outer, Inner]("inner")
+        val nameL: Lens[Outer, String] = Lenzer.forField[Outer, String]("name")
+
+        assert(innerL.set(t, Inner("b")).inner.member === "b")
+        assert(nameL.set(t, "other name").name === "other name")
+
+        assert(nameL.mod(n => n + " mod", t).name === "name mod")
+    }
+
+
+    test("one big lenser") {
+        val t = Outer(name = "name", inner = Inner("a"))
+
+        val nameL = Lenzer.forAllFields[Outer].asInstanceOf[{val name: scalaz.Lens[Outer, String]}].name
+        assert(nameL.set(t, "other name").name === "other name")
+        assert(nameL.mod(n => n + " mod", t).name === "name mod")
+    }
+}
